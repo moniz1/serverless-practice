3902d4b563d5635ab2e91be9efed87a2a7f15331
maituan <tuan.mai@orientsoftware.com>
2019-10-09 16:48:48 +0700

    update_user

--- lambda-best-practice/handler.js
+++ lambda-best-practice/handler.js
@@ -13,6 +13,47 @@ async function getUsers(event) {
   };
 }
 
-module.exports = {
-  getUsers
+async function getData(event) {
+  const dbCtxt = new DbContext();
+  let table = event.pathParameters.table;
+  const { Items } = await dbCtxt.getAll({ TableName: table });
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      users: Items
+    })
+  };
 }
+
+async function updateUsers(event) {
+  const dbCtxt = new DbContext();
+  const Table = "users"
+  let userEmail = event.pathParameters.email;
+  let body = JSON.parse(event.body);
+
+  let params = {
+    TableName: Table,
+    Key: {
+      email: userEmail
+    },
+    UpdateExpression: "set firstname = :f, lastname=:l",
+    ExpressionAttributeValues: {
+      ':f': body.firstname,
+      ':l': body.lastname
+    },
+    ReturnValues: "UPDATED_NEW"
+  }
+  await dbCtxt.updateUser(params)
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message:`update successfully for email ${userEmail}`
+    })
+  };
+}
+
+module.exports = {
+  getUsers,
+  updateUsers,
+  getData
+};
--- lambda-best-practice/layers/db.js
+++ lambda-best-practice/layers/db.js
@@ -3,8 +3,8 @@ const AWS = require('aws-sdk');
 class DbContext {
   constructor() {
     this.db =  new AWS.DynamoDB.DocumentClient({
-      region: 'localhost',
-      endpoint: 'http://localhost:8000/',
+      region: 'http://192.168.72.118',
+      endpoint: 'http://192.168.72.118:8000',
       accessKeyId: 'DEFAULT_ACCESS_KEY',  // needed if you don't have aws credentials at all in env
       secretAccessKey: 'DEFAULT_SECRET' // needed if you don't have aws credentials at all in env
     });
@@ -12,6 +12,15 @@ class DbContext {
 
   async getAll(params) {
     return this.db.scan(params).promise();
+  }
+  async updateUser(params) {
+    return this.db.update(params,function (err, data) {
+      if (err) {
+        // console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+      } else {
+        // console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+      }
+    }).promise();
   }
 }
 
